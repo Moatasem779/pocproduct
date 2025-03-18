a0ca485a964c2f0bae287906c1317eb3b8d76e49
Islam yasser <Islam.yasser@totplatform.com>
2025-03-18 23:27:02 +0200

    add test cases

---
+++ modules/BaseProductModule/test/BaseProductModule.Application.Tests/BaseProduct/BaseProductsAppService_Tests.cs
@@ -0,0 +1,39 @@
+﻿using BaseProductModule.BaseProducts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Modularity;
+using Xunit;
+
+namespace BaseProductModule.BaseProduct;
+
+public class BaseProductsAppService_Tests<TStartupModule> : BaseProductModuleApplicationTestBase<TStartupModule>
+        where TStartupModule : IAbpModule
+
+{
+
+    private readonly IBaseProductAppService _baseProductAppService;
+
+    public BaseProductsAppService_Tests()
+    {
+        _baseProductAppService = GetRequiredService<IBaseProductAppService>();
+    }
+
+
+    [Fact]
+    public async Task GetListOfBaseProductCount()
+    {
+        var result =await _baseProductAppService.GetListAsync(new PagedAndSortedResultRequestDto
+        {
+            MaxResultCount = 10000,
+            SkipCount = 0
+
+        });
+        Assert.Equal(3, result.TotalCount);
+
+    }
+
+}
--- modules/BaseProductModule/test/BaseProductModule.TestBase/BaseProductModuleDataSeedContributor.cs
+++ modules/BaseProductModule/test/BaseProductModule.TestBase/BaseProductModuleDataSeedContributor.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using BaseProductModule.Entities;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Guids;
 using Volo.Abp.MultiTenancy;
 
@@ -10,23 +14,33 @@ public class BaseProductModuleDataSeedContributor : IDataSeedContributor, ITrans
 {
     private readonly IGuidGenerator _guidGenerator;
     private readonly ICurrentTenant _currentTenant;
+    private readonly IRepository<BaseProduct, int> _productRepostory;
+
 
     public BaseProductModuleDataSeedContributor(
-        IGuidGenerator guidGenerator, ICurrentTenant currentTenant)
+        IGuidGenerator guidGenerator, ICurrentTenant currentTenant, IRepository<BaseProduct, int> productRepostory)
     {
         _guidGenerator = guidGenerator;
         _currentTenant = currentTenant;
+        _productRepostory = productRepostory;
     }
 
-    public Task SeedAsync(DataSeedContext context)
+    public async Task SeedAsync(DataSeedContext context)
     {
         /* Instead of returning the Task.CompletedTask, you can insert your test data
          * at this point!
          */
-
-        using (_currentTenant.Change(context?.TenantId))
+        if(! await _productRepostory.AnyAsync())
         {
-            return Task.CompletedTask;
+            var products = new List<BaseProduct>{
+                new BaseProduct { Name = "Laptop", Description = "High-end gaming laptop", Price = 1500.99m },
+                new BaseProduct { Name = "Smartphone", Description = "Latest smartphone with AI features", Price = 999.99m },
+                new BaseProduct { Name = "Tablet", Description = "Lightweight tablet for work and play", Price = 499.49m },
+
+            };
+            await _productRepostory.InsertManyAsync(products);
+
         }
+        
     }
 }
