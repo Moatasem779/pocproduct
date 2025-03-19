7502aeae4f2b1c2cb61a7aea94fc84f6dc6ac4b0
moatasem mamdouh <moatasem.mamdouh66@gmail.com>
2025-03-19 12:58:19 +0200

    add physical product controller

--- modules/PhysicalProductModule/src/PhysicalProductModule.HttpApi/Samples/SampleController.cs
+++ modules/PhysicalProductModule/src/PhysicalProductModule.HttpApi/Samples/SampleController.cs
@@ -1,33 +1,33 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Volo.Abp;
+﻿//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Volo.Abp;
 
-namespace PhysicalProductModule.Samples;
+//namespace PhysicalProductModule.Samples;
 
-[Area(PhysicalProductModuleRemoteServiceConsts.ModuleName)]
-[RemoteService(Name = PhysicalProductModuleRemoteServiceConsts.RemoteServiceName)]
-[Route("api/PhysicalProductModule/sample")]
-public class SampleController : PhysicalProductModuleController, ISampleAppService
-{
-    private readonly ISampleAppService _sampleAppService;
+//[Area(PhysicalProductModuleRemoteServiceConsts.ModuleName)]
+//[RemoteService(Name = PhysicalProductModuleRemoteServiceConsts.RemoteServiceName)]
+//[Route("api/PhysicalProductModule/sample")]
+//public class SampleController : PhysicalProductModuleController, ISampleAppService
+//{
+//    private readonly ISampleAppService _sampleAppService;
 
-    public SampleController(ISampleAppService sampleAppService)
-    {
-        _sampleAppService = sampleAppService;
-    }
+//    public SampleController(ISampleAppService sampleAppService)
+//    {
+//        _sampleAppService = sampleAppService;
+//    }
 
-    [HttpGet]
-    public async Task<SampleDto> GetAsync()
-    {
-        return await _sampleAppService.GetAsync();
-    }
+//    [HttpGet]
+//    public async Task<SampleDto> GetAsync()
+//    {
+//        return await _sampleAppService.GetAsync();
+//    }
 
-    [HttpGet]
-    [Route("authorized")]
-    [Authorize]
-    public async Task<SampleDto> GetAuthorizedAsync()
-    {
-        return await _sampleAppService.GetAsync();
-    }
-}
+//    [HttpGet]
+//    [Route("authorized")]
+//    [Authorize]
+//    public async Task<SampleDto> GetAuthorizedAsync()
+//    {
+//        return await _sampleAppService.GetAsync();
+//    }
+//}
