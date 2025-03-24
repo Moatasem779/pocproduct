64114411e3fdc75dc237bc38ad239efa0b3c8ede
moatasem mamdouh <moatasem.mamdouh66@gmail.com>
2025-03-24 09:47:43 +0200

    add xml comment to mapping base product

--- modules/BaseProductModule/src/BaseProductModule.Application/BaseProductModuleApplicationAutoMapperProfile.cs
+++ modules/BaseProductModule/src/BaseProductModule.Application/BaseProductModuleApplicationAutoMapperProfile.cs
@@ -4,28 +4,52 @@ using BaseProductModule.Entities;
 
 namespace BaseProductModule;
 
+/// <summary>
+/// AutoMapper profile for mapping between BaseProduct-related DTOs and entities.
+/// </summary>
 public class BaseProductModuleApplicationAutoMapperProfile : Profile
 {
+    /// <summary>
+    /// Constructor of the <see cref="BaseProductModuleApplicationAutoMapperProfile"/> class.
+    /// Defines mapping configurations for BaseProduct entities and DTOs.
+    /// </summary>
     public BaseProductModuleApplicationAutoMapperProfile()
     {
-
+        /// <summary>
+        /// Maps <see cref="CreateUpdateBaseProductDto"/> to <see cref="BaseProductDto"/>.
+        /// Ignores audit properties and identity fields.
+        /// </summary>
         CreateMap<CreateUpdateBaseProductDto, BaseProductDto>()
              .ForMember(dest => dest.LastModificationTime, opt => opt.Ignore())
              .ForMember(dest => dest.LastModifierId, opt => opt.Ignore())
-            .ForMember(dest => dest.CreationTime, opt => opt.Ignore())
-            .ForMember(dest => dest.CreatorId, opt => opt.Ignore())
-            .ForMember(dest => dest.Id, opt => opt.Ignore()).ReverseMap();
+             .ForMember(dest => dest.CreationTime, opt => opt.Ignore())
+             .ForMember(dest => dest.CreatorId, opt => opt.Ignore())
+             .ForMember(dest => dest.Id, opt => opt.Ignore())
+             .ReverseMap();
+
+        /// <summary>
+        /// Maps <see cref="CreateUpdateBaseProductDto"/> to <see cref="BaseProduct"/>.
+        /// Ignores soft-delete and audit properties.
+        /// </summary>
         CreateMap<CreateUpdateBaseProductDto, BaseProduct>()
             .ForMember(dest => dest.IsDeleted, opt => opt.Ignore())
             .ForMember(dest => dest.DeleterId, opt => opt.Ignore())
             .ForMember(dest => dest.DeletionTime, opt => opt.Ignore())
-             .ForMember(dest => dest.LastModificationTime, opt => opt.Ignore())
+            .ForMember(dest => dest.LastModificationTime, opt => opt.Ignore())
             .ForMember(dest => dest.CreationTime, opt => opt.Ignore())
             .ForMember(dest => dest.CreatorId, opt => opt.Ignore())
             .ForMember(dest => dest.LastModifierId, opt => opt.Ignore())
             .ForMember(dest => dest.Discriminator, opt => opt.Ignore())
-            .ForMember(dest => dest.Id, opt => opt.Ignore()).ReverseMap();
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ReverseMap();
+
+        /// <summary>
+        /// Maps <see cref="BaseProduct"/> to <see cref="BaseProductDto"/>.
+        /// Includes extra property mappings.
+        /// </summary>
         CreateMap<BaseProduct, BaseProductDto>()
-            .MapExtraProperties().ReverseMap() ;
+            .MapExtraProperties()
+            .ReverseMap();
     }
 }
+
